d1cb9bb0dce3a7925ba0f0e9ff6c8193a48873e0
Daniel Xu <daniel.xu.23@bishops.com>
2021-04-17 15:38:58 -0700

    purple done!

--- mysite/games/models.py
+++ mysite/games/models.py
@@ -4,20 +4,23 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile (models.Model):
+    numWins = models.IntegerField(default = 0)
+    numLosses = models.IntegerField(default = 0)
+    numDraws = models.IntegerField(default = 0)
+
     user = models.OneToOneField(
         User,
-        # numWins = models.IntegerField,
-        # numLosses = models.IntegerField,
-        # numDraws = models.IntegerField,
         on_delete = models.CASCADE,
-        primary_key = True
+        primary_key = True,
     )
     def __str__(self):
-        return self.user
-    # def numGames(self):
-    #     return numWins + numDraws + numLosses
-    # def winLossRation(self):
-    #     return numWins / numLosses
+        return self.user.username
+    def numGames(self):
+        return numWins + numDraws + numLosses
+    def winLossRation(self):
+        if numLosses != 0:
+            return numWins / numLosses
+        return float('inf')
 
 # multiple games = multiple models
 
@@ -25,14 +28,16 @@ class Match(models.Model):
     # ForeignKey = one-to-many
     player1 = models.ForeignKey(
         User,
-        on_delete,
+        on_delete = models.CASCADE,
+        related_name = 'player1',
     )
     player2 = models.ForeignKey(
         User,
-        on_delete,
+        on_delete = models.CASCADE,
+        related_name = 'player2',
     )
 
-    PLAYER_1_WINS = models.BooleanField
+    PLAYER_1_WINS = models.BooleanField(default = False)
 
     def __str__(self):
-        return self.player1 + " vs. " + self.player2
+        return self.player1.username + " vs. " + self.player2.username
--- mysite/games/views.py
+++ mysite/games/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Profile, Match
 
 
 # Weite views here
 def home(request):
     # return HttpResponse("Welcome to the home page!")
-    return render(request, 'games/index.html', {})
+    allMatches = Match.objects.all()
+    allProfiles = Profile.objects.all() #like a list
+    context = {'allMatches': allMatches,
+    'allProfiles': allProfiles,
+    }
+    return render(request, 'games/index.html', context)
 
 def game(request):
     # return HttpResponse("Hello, world. You're at the game.")
