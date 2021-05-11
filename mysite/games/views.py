c45caac58c7c02f681c90093fb250c937c1949ba
Daniel Xu <daniel.xu.23@bishops.com>
2021-05-10 22:02:35 -0700

    Wrote some user authentication stuff, need to debug.

--- mysite/games/views.py
+++ mysite/games/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Profile, Match
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+import pprint
+
 
 
 # Write views here
 def home(request):
+    if request.POST:
+        if 'inputUsername' in request.POST.keys():
+            user = authenticate(username=request.POST['inputUsername'],
+            password=request.POST['inputPassword'])
+            if user is not None:
+                login(request, user)
+            else:
+                print("Failed login.")
+        elif 'logout' in request.POST.keys():
+            logout(request)
+
+    if request.user.is_authenticated:
+        loggedIn = True;
+    else:
+        loggedIn = False;
+
+
     # return HttpResponse("Welcome to the home page!")
     allMatches = Match.objects.all()
     allProfiles = Profile.objects.all() #like a list
