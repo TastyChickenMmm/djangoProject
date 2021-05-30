140ce2a2309a501d1927fd5e55c82ec64cc28389
Daniel Xu <daniel.xu.23@bishops.com>
2021-05-29 23:09:06 -0700

    Added a game_pending page where the requester can wait for the game to be accepted. This page's view will also create the Match object.

--- mysite/games/urls.py
+++ mysite/games/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('game/', views.game, name='game'),
+    path('game_pending/', views.game_pending, name='game_pending'),
     path('profile/', views.profile, name='profile'),
 ]
--- mysite/games/views.py
+++ mysite/games/views.py
@@ -15,25 +15,21 @@ def home(request):
             user = authenticate(username=request.POST['inputUsername'],
             password=request.POST['inputPassword'])
             if user is not None:
-                print("Login successful.")
                 login(request, user)
-            else:
-                print("Failed login.")
         elif 'logout' in request.POST.keys():
             logout(request)
 
     if request.user.is_authenticated:
         loggedIn = True;
-        print("loggedIn = True")
     else:
         loggedIn = False;
-        print("loggedIn = False")
 
 
     # return HttpResponse("Welcome to the home page!")
     allMatches = Match.objects.all()
     for match in allMatches:
-        print(match.player2.is_anonymous)
+        print("Another match between " + match.player1.username + " and " + match.player2.username)
+        print(match.player2.username == "Dummy")
     allProfiles = Profile.objects.all() #like a list
     context = {'allMatches': allMatches,
     'allProfiles': allProfiles,
@@ -41,30 +37,27 @@ def home(request):
     }
     return render(request, 'games/index.html', context)
 
-def game(request):
-    # return HttpResponse("Hello, world. You're at the game.")
+def game_pending(request):
     context = {'match':[]}
 
     # Create match if user is game creator.
     if 'request_game' in request.GET.keys():
         dummyUser = User.objects.get(username = "Dummy")
         newMatch = Match.objects.get_or_create(player1=request.user, player2=dummyUser, creator=request.user)
-        print(request.user)
         context['request_match'] = newMatch
 
-    # Find match if user is game acceptor.
-    elif 'matchpk' in request.GET.keys():
+    return render(request, 'games/game_pending.html', context)
+
+def game(request):
+    context = {'match':[]}
+
+    if 'matchpk' in request.GET.keys():
         print(request.GET['matchpk'])
         matchpk = request.GET['matchpk']
         match = Match.objects.filter(pk = matchpk)[0]
         match.player2 = request.user
         match.save()
-        print("PLAYER 2 HAS BEEN CHANGED TOOOOOOOOO")
-        print(match.player2)
         context['match'] = match
-        # match.player1 = match.creator
-    else:
-        print("MATCH NOT FOUND")
 
     return render(request, 'games/game.html', context)
 
